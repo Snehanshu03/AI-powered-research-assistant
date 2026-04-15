ae328ff12d3b9a50936539a3672d9fab54a9ec9e
Snehanshu03 <157910254+Snehanshu03@users.noreply.github.com>
2026-04-15 22:36:55 +0530

    remove .next cache

--- frontend/.next/dev/static/chunks/app_components_PDFViewer_tsx_0d5243c6._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_87a34d08._.js",
-  "static/chunks/app_components_PDFViewer_tsx_5952804e._.js",
-  "static/chunks/node_modules_react-pdf_dist_Page_9ecb989f._.css"
-],
-    source: "dynamic"
-});
--- frontend/.next/dev/static/chunks/frontend_app_components_PDFViewer_tsx_cbc4de81._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/9e883_bd267577._.js",
-  "static/chunks/frontend_app_components_PDFViewer_tsx_88dd2d47._.js",
-  "static/chunks/9e883_react-pdf_dist_Page_387926c7._.css"
-],
-    source: "dynamic"
-});
